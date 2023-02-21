0013ff8b27a0b463bf7d6f2b943329f35d10729d
i44p <jsbbdjzjsjawk+@gmail.com>
2023-02-21 10:51:28 +0300

    add gcd implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 // Copyright 2021 NNTU-CS
 #include "alg.h"
+#include <iostream>
 
-int main() {
+int main()
+{
     int result = gcd(10, 15);
+    std::cout << result << std::endl;
     return 0;
 }
