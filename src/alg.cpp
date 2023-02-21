1b2f4884fd8c543aaf0e3e1253265262b7e097ad
i44p <jsbbdjzjsjawk+@gmail.com>
2023-02-21 13:35:04 +0300

    fix lint

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,23 +1,18 @@
 // Copyright 2021 NNTU-CS
 
-int max(const int &a, const int &b)
-{
-    if (a > b)
-        return a;
-    return b;
+int max(const int &a, const int &b) {
+  if (a > b) return a;
+  return b;
 }
 
-int min(const int &a, const int &b)
-{
-    if (a < b)
-        return a;
-    return b;
+int min(const int &a, const int &b) {
+  if (a < b) return a;
+  return b;
 }
 
-int gcd(int a, int b)
-{
-    int mi = min(a, b);
-    int mod = max(a, b) % mi;
+int gcd(int a, int b) {
+  int mi = min(a, b);
+  int mod = max(a, b) % mi;
 
-    return mod ? gcd(mod, mi) : mi;
+  return mod ? gcd(mod, mi) : mi;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
-#include "alg.h"
 #include <iostream>
 
-int main()
-{
-    int result = gcd(10, 15);
-    std::cout << result << std::endl;
-    return 0;
+#include "alg.h"
+
+int main() {
+  int result = gcd(10, 15);
+  std::cout << result << std::endl;
+  return 0;
 }
